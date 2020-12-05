a417c13c306776954bac50dbbf94c849e88767a3
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-05 20:11:38 +0900

    순수 JPA 페이징과 정렬

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -90,4 +90,26 @@ class MemberJpaRepositoryTest {
 
         assertThat(findMember).isEqualTo(m1);
     }
+
+    @Test
+    void paging() {
+        //given
+        memberJpaRepository.save(new Member("memeber1", 10));
+        memberJpaRepository.save(new Member("memeber2", 10));
+        memberJpaRepository.save(new Member("memeber3", 10));
+        memberJpaRepository.save(new Member("memeber4", 10));
+        memberJpaRepository.save(new Member("memeber5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        //when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        //then
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
+    }
 }
