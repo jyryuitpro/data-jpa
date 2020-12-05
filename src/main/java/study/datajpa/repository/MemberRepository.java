dbd391aa29cdd3d941c7afbeacdc94b3c4643c79
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-05 21:14:50 +0900

    스프링 데이터 JPA 페이징과 정렬

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,8 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -36,4 +39,12 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findMemberByUsername(String username); //단건
 
     Optional<Member> findOptionalByUsername(String username); //단건 Optional
+
+//    @Query(value = "select m from Member m left join m.team t", countQuery = "select count(m) from Member m")
+    @Query(value = "select m from Member m")
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    Slice<Member> findSliceByAge(int age, Pageable pageable);
+
+    List<Member> findListByAge(int age, Pageable pageable);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,10 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
@@ -174,5 +178,106 @@ class MemberRepositoryTest {
         //NonUniqueResultException > IncorrectResultSizeDataAccessException
         Optional<Member> bbb = memberRepository.findOptionalByUsername("AAA");
         System.out.println("findMember = " + bbb);
+    }
+
+    @Test
+    void paging() {
+        //given
+        memberRepository.save(new Member("memeber1", 10));
+        memberRepository.save(new Member("memeber2", 10));
+        memberRepository.save(new Member("memeber3", 10));
+        memberRepository.save(new Member("memeber4", 10));
+        memberRepository.save(new Member("memeber5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        //when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+
+        Page<MemberDto> toMap = page.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+
+        //then
+        List<Member> content = page.getContent();
+        long totalElements = page.getTotalElements();
+
+        for (Member member : content) {
+            System.out.println("member = " + member);
+        }
+
+        System.out.println("totalElements = " + totalElements);
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(5);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
+    }
+
+    @Test
+    void pagingSlice() {
+        //given
+        memberRepository.save(new Member("memeber1", 10));
+        memberRepository.save(new Member("memeber2", 10));
+        memberRepository.save(new Member("memeber3", 10));
+        memberRepository.save(new Member("memeber4", 10));
+        memberRepository.save(new Member("memeber5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        //when
+        Slice<Member> page = memberRepository.findSliceByAge(age, pageRequest);
+
+        //then
+        List<Member> content = page.getContent();
+//        long totalElements = page.getTotalElements();
+
+        for (Member member : content) {
+            System.out.println("member = " + member);
+        }
+
+//        System.out.println("totalElements = " + totalElements);
+
+        assertThat(content.size()).isEqualTo(3);
+//        assertThat(page.getTotalElements()).isEqualTo(5);
+        assertThat(page.getNumber()).isEqualTo(0);
+//        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
+    }
+
+    @Test
+    void pagingList() {
+        //given
+        memberRepository.save(new Member("memeber1", 10));
+        memberRepository.save(new Member("memeber2", 10));
+        memberRepository.save(new Member("memeber3", 10));
+        memberRepository.save(new Member("memeber4", 10));
+        memberRepository.save(new Member("memeber5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        //when
+        List<Member> page = memberRepository.findListByAge(age, pageRequest);
+
+        //then
+//        List<Member> content = page.getContent();
+//        long totalElements = page.getTotalElements();
+
+//        for (Member member : content) {
+//            System.out.println("member = " + member);
+//        }
+
+//        System.out.println("totalElements = " + totalElements);
+
+//        assertThat(content.size()).isEqualTo(3);
+//        assertThat(page.getTotalElements()).isEqualTo(5);
+//        assertThat(page.getNumber()).isEqualTo(0);
+//        assertThat(page.getTotalPages()).isEqualTo(2);
+//        assertThat(page.isFirst()).isTrue();
+//        assertThat(page.hasNext()).isTrue();
     }
 }
